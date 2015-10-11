82de1dc364bc6651bb112650c2ea4dd573d21129
Melvin Lee <zy@zy.sg>
2015-10-11 23:21:40 +0800

    Add back Calculator spec file

---
+++ tests/Calculator.spec.js
@@ -0,0 +1,15 @@
+import Calculator from '../src/Calculator';
+
+describe('Calculator', () => {
+  it('should add two numbers', () => {
+    const calculator = new Calculator();
+    const sum = calculator.add(5, 2);
+    expect(sum).toBe(7);
+  });
+
+  it('should substract two numbers', () => {
+    const calculator = new Calculator();
+    const sub = calculator.sub(5, 2);
+    expect(sub).toBe(3);
+  });
+});
