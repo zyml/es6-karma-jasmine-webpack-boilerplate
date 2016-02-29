c4a837df1c308e663b25df84fddea2698b7767a0
Melvin Lee <zy@zy.sg>
2016-02-29 14:16:25 +0800

    Fix wrong regex for source context

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,5 +1,5 @@
 var testsContext = require.context('./src', true, /-test\.js$/);
 testsContext.keys().forEach(testsContext);
 
-var srcContext = require.context('./src', true, /!(-test\.js)$/);
+var srcContext = require.context('./src', true, /^((?!__tests__).)*.js$/);
 srcContext.keys().forEach(srcContext);
